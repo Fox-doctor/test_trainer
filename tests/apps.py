4ed1b1f5e668a2c616e050b21ba79d5a6f2a7f67
Fox Doctor <erso@mail.ru>
2025-05-24 22:16:22 +0500

    Удалил временный код для создания суперпользователя

--- tests/apps.py
+++ tests/apps.py
@@ -9,19 +9,3 @@ class TestsConfig(AppConfig):
         # Импорт сигналов, если они вам нужны
         import tests.signals
 
-        # Добавим временный код для создания суперпользователя.
-        # Оборачиваем весь блок в try/except, чтобы избежать ошибки, если таблицы ещё не созданы.
-        try:
-            from django.contrib.auth import get_user_model
-            User = get_user_model()
-            if not User.objects.filter(is_superuser=True).exists():
-                # Создаём суперпользователя
-                User.objects.create_superuser(
-                    username='admin',
-                    email='admin@example.com',
-                    password='adminpassword'
-                )
-                print("Superuser успешно создан.")
-        except Exception as e:
-            # Если возникает ошибка (например, таблицы еще не созданы), пропускаем создание суперпользователя.
-            print("Не удалось создать суперпользователя:", e)
